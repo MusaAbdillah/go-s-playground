80336a5bdcf4ba1e5046928cd522fb6328cc1d2c
Musa Abdillah <musa.abdillah@ottodigital.id>
2021-09-17 16:58:16 +0700

    mocking

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,4 +1,4 @@
-package mocking
+package main
 
 
 import (
@@ -7,19 +7,49 @@ import (
 	"fmt"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int 
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+const finalWord = "Go!"
+const countDownStart = 3
+
 func TestCountdown(t *testing.T) {
     buffer := &bytes.Buffer{}
+    spySleeper := &SpySleeper{}
 
-    Countdown(buffer)
+    Countdown(buffer, spySleeper)
 
     got := buffer.String()
-    want := "3"
+    want := `3
+    2
+    1
+    Go!`
 
     if got != want {
         t.Errorf("got %q want %q", got, want)
     }
+
+    if spySleeper.Calls != 4 {
+    	t.Errorf("Not enough calls to sleeper, want 4 got %d", spySleeper.Calls)
+    }
 }
 
-func Countdown(out *bytes.Buffer){
-	    fmt.Fprint(out, "3")
+func Countdown(out *bytes.Buffer, sleeper Sleeper){
+		for i := countdownStart; i>0; i-- {
+			sleeper.Sleep()
+			fmt.Fprintln(out, i)
+		}
+
+
+		sleeper.Sleep()
+	    fmt.Fprint(out, finalWord)
 }
---
+++ mocking/main.go
@@ -0,0 +1,47 @@
+package main 
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int 
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep(){
+	time.Sleep(1 * time.Second)
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+
+
+const finalWord = "Go!"
+const countDownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper){
+
+	for i:=countDownStart; i>0; i-- {
+			time.Sleep(1 * time.Second)
+			fmt.Fprintln(out, i)	
+		}
+
+		time.Sleep(1 * time.Second)
+	    fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	defaultSleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, defaultSleeper)
+}
